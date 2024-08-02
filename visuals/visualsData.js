9b76e2010141d26f4da0c40ffe612f1770a9abad
Bob <rpmullaly@gmail.com>
2024-08-02 14:58:44 -0700

    Added New Game visual data

--- visuals/visualsData.js
+++ visuals/visualsData.js
@@ -6,6 +6,11 @@ const visualsData = [
         fileNames: ['new-code.png'],
         caption: "When we look at the world, we should be open to question everything! When we find something that is not optimal, we should write new code for it!",
     },
+    {
+        visual: "New Game",
+        fileNames: ['new-game.png'],
+        caption: "How can we come together to make a New Game for humanity? 🌈",
+    },
 ];
   
   export default visualsData;
