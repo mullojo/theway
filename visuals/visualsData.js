d8fe86b59fd3e849e2793470ebff994d271a6767
Bob <rpmullaly@gmail.com>
2024-08-02 08:12:03 -0700

    Added visualsData.js for example

---
+++ visuals/visualsData.js
@@ -0,0 +1,11 @@
+// visualsData.js
+
+const visualsData = [
+    {
+        visual: "New Code",
+        fileNames: ['new-code.png'],
+        caption: "When we look at the world, we should be open to question everything! When we find something that is not optimal, we should write new code for it!",
+    },
+];
+  
+  export default visualsData;
